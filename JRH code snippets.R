813955f79d9a49db56534ddd2151bf486938f829
James Herbick <james.r.herbick@gmail.com>
2015-01-01 14:42:29 -0600

    Added assignment of variables.

--- JRH code snippets.R
+++ JRH code snippets.R
@@ -1,5 +1,11 @@
 
+# Version Control
+# C:/R/Git/cmd is the command line directory for running Git command line prompts
+
 # Running files, parts of files
 # Highlight a selection and press Ctrl+Enter
 # To run an entire file, press Ctrl+Shift+S
+
+# assigning values to variables:  
+x <- 2
 
