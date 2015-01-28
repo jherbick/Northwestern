8053eee5a7a76e58e00fe20d9b9ef77299582d37
James Herbick <james.r.herbick@gmail.com>
2015-01-28 15:05:01 -0600

    Near final code.

--- Exam 1.R
+++ Exam 1.R
@@ -1,3 +1,14 @@
+
+# Question 7
+#-----------
+# Read in the HerbickJ_Midterm.csv dataset
+fileLocation1 <- "C:/Users/James R. Herbick/Documents/Northwestern/PREDICT 401/Exam 1/HerbickJ_Midterm.csv"
+question7 <- read.table (file = fileLocation1, header = FALSE, sep = ",")
+
+# Generate the 5-number summary to determine the IQR
+summary(question7[,1])
+
+
 #Question 9
 #----------
 
@@ -29,22 +40,30 @@ SoapDataSet <- read.table (file = fileLocation, header = TRUE, sep = ",")
 # Validate full dataset per Prof. Martin instructions
 str(SoapDataSet)
 
+
+# Part a
+#-------
 # Create a histogram for the Sales variable
 ggplot(data=SoapDataSet) + geom_histogram(aes(x=sales), col="black", fill = "blue") + ggtitle("Weekly Soap Sales")
 
 # Create the 5-number summary
 summary(SoapDataSet)
 
+# Create the normal probability plot
+qqnorm(SoapDataSet[,1], main = "Normal Probability Plot - Soap.csv")
+qqline(SoapDataSet, datax = FALSE, distribution = qnorm, probs = c(0.25, 0.75), qtype=7)
+
+
+# Part b
+#-------
 # Create box and whisker plot
 ggplot(data=SoapDataSet) + geom_boxplot(aes(y=sales, x=week), outlier.colour="blue") 
 
+
+# Part c
+#-------
 # Create a scatterplot for sales and week
 ggplot(data=SoapDataSet) + geom_point(aes(y=sales, x=week)) + ggtitle("Weekly Soap Sales")
-
-
-
-
-
 
 
 
